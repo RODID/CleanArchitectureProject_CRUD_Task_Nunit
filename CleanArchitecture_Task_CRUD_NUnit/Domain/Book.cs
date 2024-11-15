d98e02555642b15c73b97cd785f05efdb10fed01
arjan <arjan@ARRU-HI>
2024-11-15 19:29:55 +0100

    logiken för CRUD finns, implementationer för test av CREATE och EAD gjorda

---
+++ CleanArchitecture_Task_CRUD_NUnit/Domain/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibrary
+{
+    public class Book
+    {
+        public Book(int id, string author, string bookName)
+        {
+            Id = id;
+            Author = author;
+            BookName = bookName;
+        }
+
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string BookName { get; set; }
+
+    }
+}
