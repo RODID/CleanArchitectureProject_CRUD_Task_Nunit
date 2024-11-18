f1492a4f203991ed2a7d038c78c9809cc3aa34eb
arjan <arjan@ARRU-HI>
2024-11-18 16:56:00 +0100

    Update! First Commit

--- CleanArchitecture_Task_CRUD_NUnit/Infrastructure/BookRepository.cs
+++ CleanArchitecture_Task_CRUD_NUnit/Infrastructure/BookRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BookRepository;
+using Infrastructure;
 using ClassLibrary;
 
 namespace Infrastructure
@@ -49,5 +49,6 @@ namespace Infrastructure
             _books.Remove(bookToDelete);
             return true;
         }
+
     }
 }
--- CleanArchitecture_Task_CRUD_NUnit/Infrastructure/IBookRepository.cs
+++ CleanArchitecture_Task_CRUD_NUnit/Infrastructure/IBookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Infrastructure
 {
     public interface IBookRepository
     {
+        Book GetById(int id);
+
+        List<Book> GetAll();
+        void Add(Book book);
+        bool Update(Book book);
+        bool Delete(int id);
+        
     }
 }
