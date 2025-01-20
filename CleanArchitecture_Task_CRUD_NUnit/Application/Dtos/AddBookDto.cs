7368c1184f022e5f6f362c915f7a1ad5b7666606
arjan <arjan@ARRU-HI>
2025-01-20 14:43:36 +0100

    update?

--- CleanArchitecture_Task_CRUD_NUnit/Application/Dtos/AddBookDto.cs
+++ CleanArchitecture_Task_CRUD_NUnit/Application/Dtos/AddBookDto.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Dtos
+﻿namespace Application.Dtos
 {
-    public class AddBookDto
+    public class AddBookDTO
     {
-        [Required(ErrorMessage = "Title is required.")]
-        [StringLength(200, ErrorMessage = "Title cannot exceed 200 characters.")]
         public string Title { get; set; }
-        [Range(1450, int.MaxValue, ErrorMessage = "Year Published must be greater than or equal to 1450.")]
-        public int YearPublished { get; set; }
-        [Required(ErrorMessage = "Description is required.")]
-        [StringLength(2000, ErrorMessage = "Description cannot exceed 2000 characters.")]
         public string Description { get; set; }
+        public int YearPublished { get; set; }
     }
 }
